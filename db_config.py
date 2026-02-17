e04e1c6aafa2898e1be31e4f54637cb2161864c1
Aitha Charvi Sree <aithacharvi.sree2022@vitstudent.ac.in>
2026-02-17 11:21:12 +0530

    Connected Render Postgres DB

--- db_config.py
+++ db_config.py
@@ -1,10 +1,15 @@
+import os
 import psycopg2
+from urllib.parse import urlparse
 
 def get_db_connection():
+    database_url = os.getenv("DATABASE_URL")
+    result = urlparse(database_url)
+
     return psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="12345",
-        port=5432
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
     )
